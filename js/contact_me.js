38122019c4b50b7a9cdde14572807f4b156b0756
Yumenio <alejandroandy447@gmail.com>
2023-10-10 14:02:42 -0400

    fix: added error handling to sendEmail function

--- js/contact_me.js
+++ js/contact_me.js
@@ -2,9 +2,10 @@ function sendEmail() {
   Email.send({
     Host: 'smtp.elasticemail.com',
     Username: 'alejandroandy447@gmail.com',
-    Password: '8B334ED7D6656FF57F23D7ADB38AEDB3397D',
+    Password: 'tbh',
     To: 'alejandroandy447@gmail.com',
-    From: document.getElementById('email').value,
+    // From: document.getElementById('email').value,
+    From: 'alejandroandy447@gmail.com',
     Subject: 'CBHH Contact Message',
     Body:
       'Name: ' +
@@ -15,8 +16,15 @@ function sendEmail() {
       document.getElementById('phone').value +
       '<br>Message: ' +
       document.getElementById('message').value,
-  }).then((message) => alert('Message Sent Succesfully'));
-  console.log('sent');
+  })
+    .then((message) => {
+      console.log('Email sent succesfully');
+      $('#successMessage').removeClass('hidden');
+    })
+    .catch((error) => {
+      console.error('Error sendind message:', error);
+      alert('Error sendind message. Please try again later.');
+    });
 }
 
 // Validation
@@ -30,13 +38,15 @@ function sendEmail() {
 //     },
 //     submitSuccess: function ($form, event) {
 //       //function sendEmail() {
-//       console.log($form);
+//       var emailValue = $form.find('input[name="email"]').val();
+//       console.log(emailValue);
 //       Email.send({
 //         Host: 'smtp.elasticemail.com',
 //         Username: 'alejandroandy447@gmail.com',
 //         Password: '8B334ED7D6656FF57F23D7ADB38AEDB3397D',
 //         To: 'alejandroandy447@gmail.com',
-//         From: document.getElementById('email').value,
+//         // From: document.getElementById('email').value,
+//         From: 'alejandroandy447@gmail.com',
 //         Subject: 'CBHH Contact Message',
 //         Body:
 //           'Name: ' +
